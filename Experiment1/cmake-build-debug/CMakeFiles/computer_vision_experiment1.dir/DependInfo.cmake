58a29a2aae5018c1f5e55d4744c6500dcc6694c7
cjr <871218940@qq.com>
2019-09-18 20:38:52 +0800

    Experiment1

---
+++ Experiment1/cmake-build-debug/CMakeFiles/computer_vision_experiment1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/chenjiarui/CLionProjects/computer_vision_experiment1/alpha合成.cpp" "/Users/chenjiarui/CLionProjects/computer_vision_experiment1/cmake-build-debug/CMakeFiles/computer_vision_experiment1.dir/alpha合成.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/opencv/4.1.1_2/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Experiment1/cmake-build-debug/CMakeFiles/computer_vision_experiment1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/computer_vision_experiment1.dir/alpha合成.cpp.o"
+  "computer_vision_experiment1.pdb"
+  "computer_vision_experiment1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/computer_vision_experiment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
